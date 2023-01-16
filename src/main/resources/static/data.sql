00f45d4a21fcddbffa725675ff92e927636ba27b
abdelrahman abu hilal <AbdelrahmanA@exalt.ps>
2023-01-16 15:55:47 +0200

    monday check

--- src/main/resources/static/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO Product VALUES ('xbox',40,3200);
---
+++ src/test/java/com/example/shoppingcart/repository/CustomerRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.example.shoppingcart.repository;
+
+import com.example.shoppingcart.entity.Customer;
+import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@SpringJUnitConfig
+class CustomerRepositoryTest {
+
+//    @MockBean
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+
+//    @ParameterizedTest
+//    @ValueSource(strings = { "abd@gmail.com", "john@gmail.com" })
+
+    @Test
+    public void itShouldGetCustomerByHisEmailAndName() {
+        //given
+        String name="Abed";
+        String email="abd.hilal14@gmail.com";
+        Customer customer=new Customer(name,email);
+        customerRepository.save(customer);
+        //when
+        Customer customer1= customerRepository.getCustomerByEmailAndName(email,name);
+        assertEquals(customer1,customer);
+    }
+
+
+
+
+
+}
